09e11b39dceeeae6053dff6f9f1659efa0ae183a
Shayne Sweeney <shaynesweeney@me.com>
2016-02-13 12:33:24 -0800

    Update example

--- example/main.go
+++ example/main.go
@@ -11,32 +11,39 @@ import (
 
 var lu logupdate.LogUpdate
 
-func render(tick <-chan time.Time) {
-	i, frames := 0, []string{"-", "\\", "|", "/"}
+func startRenderer() {
+	i := 0
+	tick := time.Tick(80 * time.Millisecond)
 	for {
 		select {
 		case <-tick:
-			i++ // increment frame
-			frame := frames[i%len(frames)]
-			lu.Render(fmt.Sprintf(
-				`
-               ♥♥
-          %s unicorns %s
-               ♥♥
-				`, frame, frame))
+			i++
+			render(i)
 		}
 	}
 }
 
+func render(i int) {
+	frames := []string{"-", "\\", "|", "/"}
+	frame := frames[i%len(frames)]
+	lu.Render(fmt.Sprintf(
+		`
+               ♥♥
+          %s unicorns %s
+               ♥♥
+				`, frame, frame))
+}
+
 func main() {
 	lu = logupdate.LogUpdate{}
-	tick := time.Tick(80 * time.Millisecond)
-	go render(tick)
+	defer func() {
+		lu.Clear()
+		lu.Done()
+	}()
+
+	go startRenderer()
 
 	brk := make(chan os.Signal)
 	signal.Notify(brk, os.Interrupt)
 	<-brk // wait for Ctrl-c
-
-	lu.Clear()
-	lu.Done()
 }
